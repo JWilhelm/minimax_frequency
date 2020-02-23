31076316e55a68e34853c0edc1828cc899dbbd01
Jan Wilhelm <wij17778@pc1011600161.uni-regensburg.de>
2020-02-23 14:03:22 +0100

    proper grid for x; remove some printing, Remez algorithm running through but the result of Remez is still wrong

--- minimax_frequency.py
+++ minimax_frequency.py
@@ -13,12 +13,11 @@ def main():
     n_x       = 1000                   # total number of points on the x-axis for optimization
     eps_diff  = 10**(-10)
 
-    xdata = 10**(np.logspace(0,np.log(np.log10(R_minimax)),n_x))/10
+    xdata = 10**(np.logspace(0,np.log10(np.log10(R_minimax*10)),n_x))/10
     ydata = np.zeros(n_x)
 
     alphas_betas_init = np.loadtxt("alpha_beta_of_N_"+str(n_minimax))
 
-    print("alphas_betas_init",alphas_betas_init[0:n_minimax])
     print("xdata", xdata)
 
     denominator_matrix = build_denominator_matrix(xdata, alphas_betas_init[0:n_minimax])
@@ -26,12 +25,6 @@ def main():
     print("np.shape(denominator_matrix)", np.shape(denominator_matrix))
 
     alphas_betas_L2_opt, alphas_betas_conv = curve_fit(eta, xdata, ydata, p0=alphas_betas_init)
-
-    fig1, (axis1) = pl.subplots(1,1)
-    axis1.set_xlim((0.8,R_minimax))
-    axis1.semilogx(xdata,eta_plotting(xdata,alphas_betas_L2_opt))
-
-    pl.show()
 
     alphas_betas_E = np.append(alphas_betas_L2_opt,1)
 
@@ -61,8 +54,8 @@ def main():
     axis1.set_xlim((0.8,R_minimax))
     axis1.semilogx(xdata,eta_plotting(xdata,alphas_betas_L2_opt))
     axis1.semilogx(xdata,eta_plotting(xdata,alphas_betas_E))
-    axis1.semilogx([0.8,R_minimax], [alphas_betas_E[-1],alphas_betas_E[-1]])
-    axis1.semilogx([0.8,R_minimax], [-alphas_betas_E[-1],-alphas_betas_E[-1]])
+#    axis1.semilogx([0.8,R_minimax], [alphas_betas_E[-1],alphas_betas_E[-1]])
+#    axis1.semilogx([0.8,R_minimax], [-alphas_betas_E[-1],-alphas_betas_E[-1]])
 
     pl.show()
 
@@ -80,7 +73,6 @@ def eta_plotting(x, *params):
     params_1d = np.transpose(params)[:,0]
     denominator_matrix = build_denominator_matrix(x, np.array(params_1d[0:np.size(params)//2]))
     return 1/x - np.array(denominator_matrix).dot(params_1d[np.size(params)//2:(np.size(params)//2)*2])
-#    return 1/x - (np.exp(-outer(x,params_1d[0:np.size(params)//2]))).dot(params_1d[np.size(params)//2:(np.size(params)//2)*2])
 
 def eta_for_alphas_betas_E_update(x, *params):
     params_1d = np.transpose(params)[:,0]
@@ -90,10 +82,7 @@ def eta_for_alphas_betas_E_update(x, *params):
     E[0:size_x//2+1] = x[size_params-1]
     E[size_x//2+1:] = -x[size_params-1]
     denominator_matrix = build_denominator_matrix(params_1d,np.array(x[0:np.size(x)//2]))
-    print("shape denom matrix",np.shape(denominator_matrix))
     return 1/params_1d - np.array(denominator_matrix).dot(x[np.size(x)//2:(np.size(x)//2)*2])
-
-#    return 1/params_1d - (np.exp(-outer(params_1d,x[0:np.size(x)//2]))).dot(x[np.size(x)//2:np.size(x)-1]) - E
 
 if __name__ == "__main__":
     main()
