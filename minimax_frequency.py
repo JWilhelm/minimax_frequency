c43aad182822dd46cedaec9b8a0af2a08c46efc5
Jan Wilhelm <wij17778@pc1011600161.uni-regensburg.de>
2020-02-23 13:18:36 +0100

    L2 optimization runs through but there is no optimized result yet

--- minimax_frequency.py
+++ minimax_frequency.py
@@ -29,26 +29,26 @@ def main():
     alphas_betas_E = np.append(alphas_betas_L2_opt,1)
 #    alphas_betas_E = np.append(alphas_betas_init,1)
 
-    E_old = alphas_betas_E[-1]*2
-
-    sort_indices = np.argsort(alphas_betas_E[0:n_minimax])
-    np.savetxt("alpha_beta_of_N_"+str(n_minimax)+"_L2", np.append(alphas_betas_E[sort_indices],alphas_betas_E[sort_indices+n_minimax]) )
-    i = 0
-    while (alphas_betas_E[-1]/E_old < 1-eps_diff or alphas_betas_E[-1] > E_old):
-
-        E_old = alphas_betas_E[-1]
-        extrema_x = np.append(xdata[0], xdata[argrelextrema(eta_plotting(xdata,alphas_betas_E[0:np.size(alphas_betas_E)-1]), np.greater)[0]])
-        extrema_x = np.append(extrema_x, xdata[argrelextrema(eta_plotting(xdata,alphas_betas_E[0:np.size(alphas_betas_E)-1]), np.less)[0]])
-        print("number of extrema =", np.size(extrema_x))
-        alphas_betas_E[np.size(alphas_betas_E)-1] = np.average(np.abs(eta_plotting(extrema_x,alphas_betas_E[0:np.size(alphas_betas_E)-1])))
-        i += 1
-        print("iteration =", i, "E =",  alphas_betas_E[-1])
-        print("iteration =", i, "alphas_betas_E =",  alphas_betas_E)
-
-        alphas_betas_E = fsolve(eta_for_alphas_betas_E_update, x0=alphas_betas_E, args=extrema_x)
-
-    sort_indices = np.argsort(alphas_betas_E[0:n_minimax])
-    np.savetxt("alpha_beta_of_N_"+str(n_minimax), np.append(alphas_betas_E[sort_indices],alphas_betas_E[sort_indices+n_minimax]) )
+#    E_old = alphas_betas_E[-1]*2
+#
+#    sort_indices = np.argsort(alphas_betas_E[0:n_minimax])
+#    np.savetxt("alpha_beta_of_N_"+str(n_minimax)+"_L2", np.append(alphas_betas_E[sort_indices],alphas_betas_E[sort_indices+n_minimax]) )
+#    i = 0
+#    while (alphas_betas_E[-1]/E_old < 1-eps_diff or alphas_betas_E[-1] > E_old):
+#
+#        E_old = alphas_betas_E[-1]
+#        extrema_x = np.append(xdata[0], xdata[argrelextrema(eta_plotting(xdata,alphas_betas_E[0:np.size(alphas_betas_E)-1]), np.greater)[0]])
+#        extrema_x = np.append(extrema_x, xdata[argrelextrema(eta_plotting(xdata,alphas_betas_E[0:np.size(alphas_betas_E)-1]), np.less)[0]])
+#        print("number of extrema =", np.size(extrema_x))
+#        alphas_betas_E[np.size(alphas_betas_E)-1] = np.average(np.abs(eta_plotting(extrema_x,alphas_betas_E[0:np.size(alphas_betas_E)-1])))
+#        i += 1
+#        print("iteration =", i, "E =",  alphas_betas_E[-1])
+#        print("iteration =", i, "alphas_betas_E =",  alphas_betas_E)
+#
+#        alphas_betas_E = fsolve(eta_for_alphas_betas_E_update, x0=alphas_betas_E, args=extrema_x)
+#
+#    sort_indices = np.argsort(alphas_betas_E[0:n_minimax])
+#    np.savetxt("alpha_beta_of_N_"+str(n_minimax), np.append(alphas_betas_E[sort_indices],alphas_betas_E[sort_indices+n_minimax]) )
 
     fig1, (axis1) = pl.subplots(1,1)
     axis1.set_xlim((0.8,R_minimax))
@@ -62,13 +62,12 @@ def main():
 def build_denominator_matrix(xdata,alphas):
     matrix = []
     for x in xdata:
-      matrix.append((2*x/(x**2+alphas**2))**2)
+      matrix.append((2*x/(x**2+alphas**2))**2/np.pi)
     return matrix
 
 def eta(x, *params):
-    denominator_matrix = build_denominator_matrix(x, params[0:np.size(params)//2])
-    print("func", 1/x - denominator_matrix.dot(params[np.size(params)//2:]) )
-    return 1/x - denominator_matrix.dot(params[np.size(params)//2:])
+    denominator_matrix = build_denominator_matrix(x, np.array(params[0:np.size(params)//2]))
+    return 1/x - np.array(denominator_matrix).dot(params[np.size(params)//2:])
 
 def eta_plotting(x, *params):
     params_1d = np.transpose(params)[:,0]
